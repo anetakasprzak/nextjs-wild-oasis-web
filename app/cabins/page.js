7f180b77085c3e48dbbc65ad737e0444eba4fcfe
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-05-21 16:46:59 +0100

    cabins filter jsx, functionality and url update

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,8 +1,9 @@
 import CabinList from "../_components/CabinList";
 import { Suspense } from "react";
 import Spinner from "../_components/Spinner";
+import Filter from "../_components/Filter";
 
-export const revalidate = 3600;
+// export const revalidate = 3600;
 // export const revalidate = 15;
 
 export const metadata = {
@@ -25,6 +26,10 @@ export default function Page({ searchParams }) {
         home away from home. The perfect spot for a peaceful, calm vacation.
         Welcome to paradise.
       </p>
+
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
 
       <Suspense fallback={<Spinner />}>
         <CabinList filter={filter} />
