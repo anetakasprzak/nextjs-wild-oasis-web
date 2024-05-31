48bc90255d858693199106665110158ee3763ba1
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-05-31 11:44:19 +0100

    context api

--- app/_components/ReservationForm.js
+++ app/_components/ReservationForm.js
@@ -1,8 +1,10 @@
 "use client";
 
+import { useReservation } from "./ReservationContext";
+
 function ReservationForm({ cabin }) {
-  // CHANGE
   const { maxCapacity } = cabin;
+  const { range } = useReservation();
 
   return (
     <div className="scale-[1.01]">
@@ -20,6 +22,10 @@ function ReservationForm({ cabin }) {
           <p>{user.name}</p>
         </div> */}
       </div>
+
+      <p>
+        {String(range.from)} to {String(range.to)}
+      </p>
 
       <form className="bg-primary-900 py-10 px-16 text-lg flex gap-5 flex-col">
         <div className="space-y-2">
