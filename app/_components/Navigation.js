5f0cad44f7525dc14f6807125094e7a2115306dd
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-06-04 12:59:33 +0100

    auth.js google user signin

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -1,6 +1,10 @@
 import Link from "next/link";
+import { auth } from "../_lib/auth";
 
-export default function Navigation() {
+export default async function Navigation() {
+  const session = await auth();
+  console.log(session);
+
   return (
     <nav className="z-10 text-xl">
       <ul className="flex gap-16 items-center">
@@ -15,18 +19,33 @@ export default function Navigation() {
         <li>
           <Link
             href="/about"
-            className="hover:text-accent-400 transition-colors"
+            className="hover:text-accent-400 transition-colors "
           >
             About
           </Link>
         </li>
         <li>
-          <Link
-            href="/account"
-            className="hover:text-accent-400 transition-colors"
-          >
-            Guest area
-          </Link>
+          {session?.user?.image ? (
+            <Link
+              href="/account"
+              className="hover:text-accent-400 transition-colors flex items-center gap-3"
+            >
+              <img
+                className="h-8 rounded-full"
+                src={session.user.image}
+                alr={session.user.name}
+                referrerPolicy="no-referrer"
+              />
+              <span>Guest area</span>
+            </Link>
+          ) : (
+            <Link
+              href="/account"
+              className="hover:text-accent-400 transition-colors"
+            >
+              Guest area
+            </Link>
+          )}
         </li>
       </ul>
     </nav>
