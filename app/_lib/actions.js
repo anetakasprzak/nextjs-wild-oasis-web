64b7f07e57c2a02b106489a85c3bbe06a3e65495
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-06-10 12:29:53 +0100

    update user data

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,9 +1,18 @@
 "use server";
 
-import { signIn, signOut } from "./auth";
+import { auth, signIn, signOut } from "./auth";
 
 export async function updateGuest(formData) {
-  console.log(formData);
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  const nationalID = formData.get("nationalID");
+  const [nationality, countryFlag] = formData.get("nationality").split("%");
+
+  if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID))
+    throw new Error("Please provide valid national ID");
+
+  const updateData = { nationality, countryFlag, nationalID };
 }
 
 export async function signInAction() {
